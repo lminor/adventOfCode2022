f912f3c7934cf051e5403779e482d103a5e80196
Kyle Minor <lkm@lkminor.com>
2022-12-01 18:13:58 -0500

    update for part 2 of day1

--- day1/day1.js
+++ day1/day1.js
@@ -1,5 +1,5 @@
 var lineReader = require('readline').createInterface({
-  input: require('fs').createReadStream('day1.txt')
+  input: require('fs').createReadStream('day1/day1.txt')
 });
 
 let elf = 0;
@@ -20,7 +20,10 @@ lineReader.on('line', function (line) {
 lineReader.on('close', function () {
   console.log('Last Elf = ' + elf);
   elves.sort(function (a, b) { return a - b })
+  let finalElf3 = elves.length - 3;
+  let finalElf2 = elves.length - 2;
   let finalElf = elves.length - 1;
-  console.log("Elf with most Calories = " + finalElf)
+  console.log("Elf with most calories = " + finalElf)
   console.log("His calories = " + elves[finalElf]);
+  console.log("Last 3 elves calories combined." + elves[finalElf3] + elves[finalElf2] + elves[finalElf]);
 });
